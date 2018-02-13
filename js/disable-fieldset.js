1bf5026888083557feb41b55c1235bcbb012a944
Julie <mailtojuuu@gmail.com>
2018-02-13 11:35:34 +0200

    separate map file to many parts

---
+++ js/disable-fieldset.js
@@ -0,0 +1,13 @@
+'use strict';
+(function () {
+  // блокировка\разблокировка полей формы
+  var notice = document.querySelector('.notice');
+  var fieldsets = notice.querySelectorAll('fieldset');
+  window.disableFildset = function (boolean) {
+    for (var i = 0; i < fieldsets.length; i++) {
+      fieldsets[i].disabled = boolean;
+    }
+  };
+
+  window.disableFildset(true);
+})();
